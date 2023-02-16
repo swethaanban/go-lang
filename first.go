8b6bc8556d48602487b37286c8802b9d6a232978
swethaanban <98731710+swethaanban@users.noreply.github.com>
2023-02-16 13:56:23 +0530

    Update first.go

--- first.go
+++ first.go
@@ -1,21 +1,23 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func homePage1(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "Welcome to the HomePage!")
-    fmt.Println("Endpoint Hit: homePage")
+func homePage1(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Welcome to the HomePage!")
+	fmt.Println("Endpoint Hit: homePage")
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage1)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/", homePage1)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
+
+//test
